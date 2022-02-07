5e42b21ede9ecc3313036eef27dfac5ce243a76d
Albert Yu <kbalbertyu@gmail.com>
2022-02-07 14:25:54 +0800

    Feature: 1.0-2022-02-07
    更新内容:
    1. 修改Apps Script方法名

--- upgrade.bat
+++ upgrade.bat
@@ -5,4 +5,16 @@ title Upgrade Program
 set ROOT_DIR=%cd%
 echo Root directory: %ROOT_DIR%
 
-set GIT_DIR=%ROOT_DIR%\bin\PortableGit\bin
+git.exe --version
+set GIT_PATH=git.exe
+if errorlevel 0 (
+    set GIT_PATH=%ROOT_DIR%\bin\PortableGit\bin\git.exe
+)
+echo %GIT_PATH%
+%GIT_PATH% --version
+
+echo Downloading upgrade files...
+git pull origin master
+
+echo Program files are downloaded successfully
+timeout 7
